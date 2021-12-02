a414b72ec968b8f8d6890a9aa1333fc1a0d42733
JC Francisco <jc.francisco17@gmail.com>
2021-12-02 14:16:57 +0800

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,39 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val measurements = ArrayDeque(input)
+        var increaseCount = 0
+        var previousMeasurement = measurements.removeFirst()
+        while (measurements.isNotEmpty()) {
+            if (measurements[0].toInt() > previousMeasurement.toInt()) {
+                increaseCount++
+            }
+            previousMeasurement = measurements.removeFirst()
+        }
+        return increaseCount
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val windowingResult = mutableListOf<String>()
+        val windowSize = 3
+        var windowSum = 0
+        var windowStart = 0
+
+        for ((windowEnd, measurement) in input.withIndex()) {
+            windowSum += measurement.toInt()
+            if (windowEnd >= windowSize - 1) {
+                windowingResult.add(windowSum.toString())
+                windowSum -= input[windowStart].toInt()
+                windowStart++
+            }
+        }
+
+        return part1(windowingResult)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
